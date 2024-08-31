2f7e8147db56aee28986425c9d1b2c3896b82fca
金子会发光 <icsjhsmqi@hotmail.com>
2024-08-31 18:24:03 +0800

    Update main.js

--- webssh/static/js/main.js
+++ webssh/static/js/main.js
@@ -3,7 +3,6 @@
 var jQuery;
 var wssh = {};
 
-
 (function() {
   // For FormData without getter and setter
   var proto = FormData.prototype,
@@ -34,6 +33,7 @@ var wssh = {};
   }
 
   document.querySelector('#sshlinkBtn').addEventListener("click", updateSSHlink);
+  document.querySelector('#secureSSHLinkBtn').addEventListener("click", buildSecureSSHLink);
 }());
 
 function updateSSHlink() {
@@ -47,7 +47,7 @@ function updateSSHlink() {
     }
     var usrnamestr = document.getElementById("username").value;
     if (usrnamestr == "") {
-      portstr = "root"
+      usrnamestr = "root"
     }
     var passwdstr = document.getElementById("password").value;
     var passwdstrAfterBase64 = window.btoa(passwdstr);
@@ -56,6 +56,34 @@ function updateSSHlink() {
     sshlinkstr = thisPageProtocol+"//"+thisPageUrl+"/?hostname="+hostnamestr+"&port="+portstr+"&username="+usrnamestr+"&password="+passwdstrAfterBase64;
 
     document.getElementById("sshlink").innerHTML = sshlinkstr;
+}
+
+function buildSecureSSHLink() {
+    var thisPageProtocol = window.location.protocol;
+    var thisPageUrl = window.location.host;
+
+    var hostnamestr = document.getElementById("hostname").value;
+    var portstr = document.getElementById("port").value;
+    if (portstr == "") {
+        portstr = "22"
+    }
+    var usrnamestr = document.getElementById("username").value;
+    if (usrnamestr == "") {
+      usrnamestr = "root"
+    }
+    var passwdstr = document.getElementById("password").value;
+    var passwdstrAfterBase64 = window.btoa(passwdstr);
+
+    var sshlinkstr;
+    sshlinkstr = thisPageProtocol+"//"+thisPageUrl+"/?hostname="+hostnamestr+"&port="+portstr+"&username="+usrnamestr+"&password="+passwdstrAfterBase64;
+
+    var resultDiv = document.getElementById("secureSSHLinkResult");
+    resultDiv.textContent = "Secure SSH Link (copy quickly and clear): " + sshlinkstr;
+    
+    // 10秒后清除链接
+    setTimeout(() => {
+        resultDiv.textContent = "Link cleared for security. Generate a new one if needed.";
+    }, 10000);
 }
 
 jQuery(function($){
@@ -414,469 +442,4 @@ jQuery(function($){
       console.log('Unable to detect the default encoding of your server');
       msg.encoding = encoding;
     } else {
-      console.log('The deault encoding of your server is ' + msg.encoding);
-    }
-
-    function term_write(text) {
-      if (term) {
-        term.write(text);
-        if (!term.resized) {
-          resize_terminal(term);
-          term.resized = true;
-        }
-      }
-    }
-
-    function set_encoding(new_encoding) {
-      // for console use
-      if (!new_encoding) {
-        console.log('An encoding is required');
-        return;
-      }
-
-      if (!window.TextDecoder) {
-        decoder = new_encoding;
-        encoding = decoder;
-        console.log('Set encoding to ' + encoding);
-      } else {
-        try {
-          decoder = new window.TextDecoder(new_encoding);
-          encoding = decoder.encoding;
-          console.log('Set encoding to ' + encoding);
-        } catch (RangeError) {
-          console.log('Unknown encoding ' + new_encoding);
-          return false;
-        }
-      }
-    }
-
-    wssh.set_encoding = set_encoding;
-
-    if (url_opts_data.encoding) {
-      if (set_encoding(url_opts_data.encoding) === false) {
-        set_encoding(msg.encoding);
-      }
-    } else {
-      set_encoding(msg.encoding);
-    }
-
-
-    wssh.geometry = function() {
-      // for console use
-      var geometry = current_geometry(term);
-      console.log('Current window geometry: ' + JSON.stringify(geometry));
-    };
-
-    wssh.send = function(data) {
-      // for console use
-      if (!sock) {
-        console.log('Websocket was already closed');
-        return;
-      }
-
-      if (typeof data !== 'string') {
-        console.log('Only string is allowed');
-        return;
-      }
-
-      try {
-        JSON.parse(data);
-        sock.send(data);
-      } catch (SyntaxError) {
-        data = data.trim() + '\r';
-        sock.send(JSON.stringify({'data': data}));
-      }
-    };
-
-    wssh.reset_encoding = function() {
-      // for console use
-      if (encoding === msg.encoding) {
-        console.log('Already reset to ' + msg.encoding);
-      } else {
-        set_encoding(msg.encoding);
-      }
-    };
-
-    wssh.resize = function(cols, rows) {
-      // for console use
-      if (term === undefined) {
-        console.log('Terminal was already destroryed');
-        return;
-      }
-
-      var valid_args = false;
-
-      if (cols > 0 && rows > 0)  {
-        var geometry = current_geometry(term);
-        if (cols <= geometry.cols && rows <= geometry.rows) {
-          valid_args = true;
-        }
-      }
-
-      if (!valid_args) {
-        console.log('Unable to resize terminal to geometry: ' + format_geometry(cols, rows));
-      } else {
-        term.on_resize(cols, rows);
-      }
-    };
-
-    wssh.set_bgcolor = function(color) {
-      set_backgound_color(term, color);
-    };
-
-    wssh.set_fontcolor = function(color) {
-      set_font_color(term, color);
-    };
-
-    wssh.custom_font = function() {
-      update_font_family(term);
-    };
-
-    wssh.default_font = function() {
-      reset_font_family(term);
-    };
-
-    term.on_resize = function(cols, rows) {
-      if (cols !== this.cols || rows !== this.rows) {
-        console.log('Resizing terminal to geometry: ' + format_geometry(cols, rows));
-        this.resize(cols, rows);
-        sock.send(JSON.stringify({'resize': [cols, rows]}));
-      }
-    };
-
-    term.onData(function(data) {
-      // console.log(data);
-      sock.send(JSON.stringify({'data': data}));
-    });
-
-    sock.onopen = function() {
-      term.open(terminal);
-      toggle_fullscreen(term);
-      update_font_family(term);
-      term.focus();
-      state = CONNECTED;
-      title_element.text = url_opts_data.title || default_title;
-      if (url_opts_data.command) {
-        setTimeout(function () {
-          sock.send(JSON.stringify({'data': url_opts_data.command+'\r'}));
-        }, 500);
-      }
-    };
-
-    sock.onmessage = function(msg) {
-      read_file_as_text(msg.data, term_write, decoder);
-    };
-
-    sock.onerror = function(e) {
-      console.error(e);
-    };
-
-    sock.onclose = function(e) {
-      term.dispose();
-      term = undefined;
-      sock = undefined;
-      reset_wssh();
-      log_status(e.reason, true);
-      state = DISCONNECTED;
-      default_title = 'WebSSH';
-      title_element.text = default_title;
-    };
-
-    $(window).resize(function(){
-      if (term) {
-        resize_terminal(term);
-      }
-    });
-  }
-
-
-  function wrap_object(opts) {
-    var obj = {};
-
-    obj.get = function(attr) {
-      return opts[attr] || '';
-    };
-
-    obj.set = function(attr, val) {
-      opts[attr] = val;
-    };
-
-    return obj;
-  }
-
-
-  function clean_data(data) {
-    var i, attr, val;
-    var attrs = form_keys.concat(['privatekey', 'passphrase']);
-
-    for (i = 0; i < attrs.length; i++) {
-      attr = attrs[i];
-      val = data.get(attr);
-      if (typeof val === 'string') {
-        data.set(attr, val.trim());
-      }
-    }
-  }
-
-
-  function validate_form_data(data) {
-    clean_data(data);
-
-    var hostname = data.get('hostname'),
-        port = data.get('port'),
-        username = data.get('username'),
-        pk = data.get('privatekey'),
-        result = {
-          valid: false,
-          data: data,
-          title: ''
-        },
-        errors = [], size;
-
-    if (!hostname) {
-      errors.push('Value of hostname is required.');
-    } else {
-      if (!hostname_tester.test(hostname)) {
-         errors.push('Invalid hostname: ' + hostname);
-      }
-    }
-
-    if (!port) {
-      port = 22;
-    } else {
-      if (!(port > 0 && port <= 65535)) {
-        errors.push('Invalid port: ' + port);
-      }
-    }
-
-    if (!username) {
-      errors.push('Value of username is required.');
-    }
-
-    if (pk) {
-      size = pk.size || pk.length;
-      if (size > key_max_size) {
-        errors.push('Invalid private key: ' + pk.name || '');
-      }
-    }
-
-    if (!errors.length || debug) {
-      result.valid = true;
-      result.title = username + '@' + hostname + ':'  + port;
-    }
-    result.errors = errors;
-
-    return result;
-  }
-
-  // Fix empty input file ajax submission error for safari 11.x
-  function disable_file_inputs(inputs) {
-    var i, input;
-
-    for (i = 0; i < inputs.length; i++) {
-      input = inputs[i];
-      if (input.files.length === 0) {
-        input.setAttribute('disabled', '');
-      }
-    }
-  }
-
-
-  function enable_file_inputs(inputs) {
-    var i;
-
-    for (i = 0; i < inputs.length; i++) {
-      inputs[i].removeAttribute('disabled');
-    }
-  }
-
-
-  function connect_without_options() {
-    // use data from the form
-    var form = document.querySelector(form_id),
-        inputs = form.querySelectorAll('input[type="file"]'),
-        url = form.action,
-        data, pk;
-
-    disable_file_inputs(inputs);
-    data = new FormData(form);
-    pk = data.get('privatekey');
-    enable_file_inputs(inputs);
-
-    function ajax_post() {
-      status.text('');
-      button.prop('disabled', true);
-
-      $.ajax({
-          url: url,
-          type: 'post',
-          data: data,
-          complete: ajax_complete_callback,
-          cache: false,
-          contentType: false,
-          processData: false
-      });
-    }
-
-    var result = validate_form_data(data);
-    if (!result.valid) {
-      log_status(result.errors.join('\n'));
-      return;
-    }
-
-    if (pk && pk.size && !debug) {
-      read_file_as_text(pk, function(text) {
-        if (text === undefined) {
-            log_status('Invalid private key: ' + pk.name);
-        } else {
-          ajax_post();
-        }
-      });
-    } else {
-      ajax_post();
-    }
-
-    return result;
-  }
-
-
-  function connect_with_options(data) {
-    // use data from the arguments
-    var form = document.querySelector(form_id),
-        url = data.url || form.action,
-        _xsrf = form.querySelector('input[name="_xsrf"]');
-
-    var result = validate_form_data(wrap_object(data));
-    if (!result.valid) {
-      log_status(result.errors.join('\n'));
-      return;
-    }
-
-    data.term = term_type.val();
-    data._xsrf = _xsrf.value;
-    if (event_origin) {
-      data._origin = event_origin;
-    }
-
-    status.text('');
-    button.prop('disabled', true);
-
-    $.ajax({
-        url: url,
-        type: 'post',
-        data: data,
-        complete: ajax_complete_callback
-    });
-
-    return result;
-  }
-
-
-  function connect(hostname, port, username, password, privatekey, passphrase, totp) {
-    // for console use
-    var result, opts;
-
-    if (state !== DISCONNECTED) {
-      console.log(messages[state]);
-      return;
-    }
-
-    if (hostname === undefined) {
-      result = connect_without_options();
-    } else {
-      if (typeof hostname === 'string') {
-        opts = {
-          hostname: hostname,
-          port: port,
-          username: username,
-          password: password,
-          privatekey: privatekey,
-          passphrase: passphrase,
-          totp: totp
-        };
-      } else {
-        opts = hostname;
-      }
-
-      result = connect_with_options(opts);
-    }
-
-    if (result) {
-      state = CONNECTING;
-      default_title = result.title;
-      if (hostname) {
-        validated_form_data = result.data;
-      }
-      store_items(fields, result.data);
-    }
-  }
-
-  wssh.connect = connect;
-
-  $(form_id).submit(function(event){
-    event.preventDefault();
-    connect();
-  });
-
-
-  function cross_origin_connect(event)
-  {
-    console.log(event.origin);
-    var prop = 'connect',
-        args;
-
-    try {
-      args = JSON.parse(event.data);
-    } catch (SyntaxError) {
-      args = event.data.split('|');
-    }
-
-    if (!Array.isArray(args)) {
-      args = [args];
-    }
-
-    try {
-      event_origin = event.origin;
-      wssh[prop].apply(wssh, args);
-    } finally {
-      event_origin = undefined;
-    }
-  }
-
-  window.addEventListener('message', cross_origin_connect, false);
-
-  if (document.fonts) {
-    document.fonts.ready.then(
-      function () {
-        if (custom_font_is_loaded() === false) {
-          document.body.style.fontFamily = custom_font.family;
-        }
-      }
-    );
-  }
-
-
-  parse_url_data(
-    decode_uri_component(window.location.search.substring(1)) + '&' + decode_uri_component(window.location.hash.substring(1)),
-    form_keys, opts_keys, url_form_data, url_opts_data
-  );
-  // console.log(url_form_data);
-  // console.log(url_opts_data);
-
-  if (url_opts_data.term) {
-    term_type.val(url_opts_data.term);
-  }
-
-  if (url_form_data.password === null) {
-    log_status('Password via url must be encoded in base64.');
-  } else {
-    if (get_object_length(url_form_data)) {
-      waiter.show();
-      connect(url_form_data);
-    } else {
-      restore_items(fields);
-      form_container.show();
-    }
-  }
-
-});
+      console.log('The deault encoding of your server is
